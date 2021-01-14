f59979844081e43b62704ec9406bba226cb8079e
Jonas Ersland <jonasersland@Jonass-MacBook-Pro.fritz.box>
2021-01-14 14:19:58 +0100

    updated shop data, updated cart.utils

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({children, isGoogleSignIn, ...otherprops}) => (
-    <button className={`${isGoogleSignIn ? 'google-btn' : ''} custom-button`} {...otherprops}>
+const CustomButton = ({children, isGoogleSignIn, inverted, ...otherprops}) => (
+    <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-btn' : ''} custom-button`} {...otherprops}>
         {children}
     </button>
 )
--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -7,8 +7,9 @@ const PreviewCollection = ({title, items}) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-            {items.filter((item,idx) => idx< 4).map(({id, ...itemProps}) => (
-                <CollectionItem key={id} {...itemProps} /> 
+            {items.filter((item,idx) => idx< 4)
+            .map(item => (
+                <CollectionItem key={item.id} item={item} /> 
             ))}
         </div>
     </div>
